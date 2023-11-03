db25a9e53c9352905bbbc193bef19ccc3a9c9d1b
Damian Thomas <dimitrisonic@gmail.com>
2023-11-03 11:36:35 -0400

    first commit

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+module.exports = {
+	plugins: [
+		// ...
+		require("tailwindcss"),
+		require("autoprefixer"),
+		// ...
+	]
+};
